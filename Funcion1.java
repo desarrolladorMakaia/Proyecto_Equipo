3f0f1d1ebf25b2dbbf72262306306b843c4ec23e
GinaLoaizaH <ginita011295@gmail.com>
2024-04-19 09:42:16 -0500

    Funcion del 1 al 10

---
+++ Funcion1.java
@@ -0,0 +1,17 @@
+public class Funcion1 {
+    public static void main(String[] args) {
+
+        // numeros del 1 al 10
+
+        int numero, i;
+
+        numero = 10;
+
+        for (i = 1; i <= numero; i++) {
+
+            System.out.println(i);
+
+        }
+
+    }
+}
